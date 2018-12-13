7f21f06f3fbde7b8062eb9a68d630fc953491dd9
rolando_r <rolando.rosalesj@gmail.com>
2018-12-13 23:36:41 +0100

    Cambios menores en la definicion en las clases de Vectores

--- HolaOpenGL/Vectores.cpp
+++ HolaOpenGL/Vectores.cpp
@@ -1,6 +1,5 @@
 #include "Vectores.h"
 
-Vec2::Vec2() {x = 0; y = 0;}
 Vec2::Vec2(GLfloat X, GLfloat Y) {x = X; y = Y;}
 GLfloat *Vec2::glVec2() {
 	GLfloat vector[2];
@@ -9,7 +8,6 @@ GLfloat *Vec2::glVec2() {
 	return vector;
 }
 
-Vec3::Vec3() { x = 0; y = 0; z = 0; }
 Vec3::Vec3(GLfloat X, GLfloat Y, GLfloat Z) { x = X; y = Y; z = Z; }
 GLfloat *Vec3::glVec3() {
 	GLfloat vector[3];
@@ -19,7 +17,6 @@ GLfloat *Vec3::glVec3() {
 	return vector;
 }
 
-Vec4::Vec4() { x = 0; y = 0; z = 0; w = 0; }
 Vec4::Vec4(GLfloat X, GLfloat Y, GLfloat Z, GLfloat W) { x = X; y = Y; z = Z; w = W; }
 GLfloat *Vec4::glVec4() {
 	GLfloat vector[4];
--- HolaOpenGL/Vectores.h
+++ HolaOpenGL/Vectores.h
@@ -1,26 +1,24 @@
 #pragma once
 #include "Tools.h"
+
 class Vec2 {
 public:
 	GLfloat x, y;
-	Vec2();
-	Vec2(GLfloat X, GLfloat Y);
+	Vec2(GLfloat X=0, GLfloat Y=0);
 	GLfloat *glVec2();
 };
 
 class Vec3 : public Vec2 {
 public:
 	GLfloat z;
-	Vec3();
-	Vec3(GLfloat X, GLfloat Y, GLfloat Z);
+	Vec3(GLfloat X=0, GLfloat Y=0, GLfloat Z=0);
 	GLfloat *glVec3();
 };
 
 class Vec4 : public Vec3 {
 public:
 	GLfloat w;
-	Vec4();
-	Vec4(GLfloat X, GLfloat Y, GLfloat Z, GLfloat W);
+	Vec4(GLfloat X=0, GLfloat Y=0, GLfloat Z=0, GLfloat W=0);
 	GLfloat *glVec4();
 };
 
